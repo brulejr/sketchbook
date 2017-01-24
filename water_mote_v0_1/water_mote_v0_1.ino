a759e73bf0f8da481ad93adf054d335008b742af
brulejr <brulejr@gmail.com>
2017-01-23 19:54:45 -0500

    Added water sensor to water_mote_v0_1

--- water_mote_v0_1/water_mote_v0_1.ino
+++ water_mote_v0_1/water_mote_v0_1.ino
@@ -6,8 +6,9 @@
    Circuit:
    * Arduino UNO
    * Ethernet shield attached to pins 10, 11, 12, 13
-   * Water sensor attached to digitial pin 2
+   * Door sensor attached to digitial pin 2
    * Temperature/Humidity (DHT11) sensor attached to digital pin 3
+   * Water sensor attached to digital pin 8
  
    Created 22-JAN-2017 by Jon Brule
 ----------------------------------------------------------------------------- */
@@ -20,14 +21,17 @@
 #define DEVICE_NAME "water_mote_01"
 #define MAC_ADDR { 0x90, 0xA2, 0xDA, 0x00, 0x4B, 0x2A }
 #define MQTT_SERVER "mqtt.autohome.brule.net"
-#define MQTT_USERID "XXXXXX"
-#define MQTT_PASSWD "XXXXXX"
+#define MQTT_USERID "pi"
+#define MQTT_PASSWD "J8n@ut0!7"
+#define WATER_PIN 8
 
 byte macAddr[] = MAC_ADDR;
 MQTT mqtt(DEVICE_NAME, macAddr, MQTT_SERVER, MQTT_USERID, MQTT_PASSWD);
 
 DHTSensor dhts(DHT_PIN);
-int doorState = digitalRead(DOOR_PIN);
+int doorState;
+
+
 
 boolean startup = true;
 
@@ -35,8 +39,13 @@ void setup() {
   Serial.begin(115200);
   mqtt.setup();
   dhts.setup();
+  
   pinMode(DOOR_PIN, INPUT_PULLUP);
+  doorState = digitalRead(DOOR_PIN);
   attachInterrupt(digitalPinToInterrupt(DOOR_PIN), doorOpen, CHANGE);
+
+  pinMode(WATER_PIN, INPUT);
+  
   delay(1000);
   startup = false;
 }
@@ -44,18 +53,22 @@ void setup() {
 void loop() {
   mqtt.check();
   readSensors();
-  delay(15000);
+  delay(10000);
+  //sleep();
 }
 
 void readSensors() {
   float temperature = dhts.temperature();
   float humidity = dhts.humidity();
+  boolean waterPresent = isExposedToWater();
 
   String json = "{";
   json += "\"temperature\": ";
   json += temperature;
   json += ", \"humidity\": ";
   json += humidity;
+  json += ", \"water\": ";
+  json += (waterPresent ? "PRESENT" : "NOT PRESENT");
   json += ", \"door\": \"";
   json += (doorState == LOW ? "OPEN" : "CLOSED");
   json += "\"}";
@@ -77,3 +90,6 @@ void doorOpen() {
   last_interrupt_time = interrupt_time;
 }
 
+boolean isExposedToWater() {
+  return (digitalRead(WATER_PIN) == LOW);
+}
